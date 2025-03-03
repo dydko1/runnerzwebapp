7c8883bdb829c119b1f7f3049806ced3d868935b
Miroslaw Dyduch <miroslaw.dyduch@gmail.com>
2025-03-03 14:51:59 +0100

    jenkinsfile 1

--- jenkins/jenkinfile1.groovy
+++ jenkins/jenkinfile1.groovy
@@ -1,7 +1,7 @@
 pipeline {
 agent any  
 parameters {  
-    string(name: 'packageType', defaultValue: 'default Miro', description: 'Type of package to build Miro')
+    string(name: 'packageType', defaultValue: 'default Miro1233')
 }  
 stages {  
     stage('Build') {  
