c3c8b148a12d57c0c44d9f7f532f3363e7a3d4d3
Miroslaw Dyduch <miroslaw.dyduch@gmail.com>
2025-03-04 17:44:37 +0100

    boolean example

--- jenkins/cred.groovy
+++ jenkins/cred.groovy
@@ -1,13 +1,14 @@
 pipeline {
     agent any
     parameters {
-        credentials(name: 'DEPLOY_KEY', description: 'SSH key for deployment')
+        password(name: 'API_TOKEN', description: 'Enter the API token for the external service')
     }
     stages {
-        stage('Deploy') {
+        stage('Call API') {
             steps {
-                withCredentials([sshUserPrivateKey(credentialsId: 'DEPLOY_KEY', keyFileVariable: 'SSH_KEY')]) {
-                    sh 'ssh -i $SSH_KEY user@server "deploy-script.sh"'
+                script {
+                    def apiToken = params.API_TOKEN
+                    sh "curl -H 'Authorization: Bearer ${apiToken}' https://api.example.com/data"
                 }
             }
         }
