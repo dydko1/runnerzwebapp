8e7e7ac5833723d8d8b3bfa8cbae2c7cb095792a
Miroslaw Dyduch <miroslaw.dyduch@gmail.com>
2025-03-04 17:37:45 +0100

    boolean example

--- jenkins/choice.groovy
+++ jenkins/choice.groovy
@@ -1,7 +1,7 @@
 pipeline{
     agent any
     parameters{
-        choice(name: 'VERSION', defaultValue: 'cccc', choices: ['aaaa', 'bbbb', 'cccc'], description: 'stages1234')
+        choice(name: 'VERSION', defaultValue: 'cccc', choices: ['aaaa', 'bbbb', 'cccc'], description: 'stages123456')
     }
     stages{
         stage('Build'){
