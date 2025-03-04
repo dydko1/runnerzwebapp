15b7cb95ab5b7e31880fcaf705c2b34b210f42be
Miroslaw Dyduch <miroslaw.dyduch@gmail.com>
2025-03-04 17:01:51 +0100

    boolean example

--- jenkins/booleanpipline.groovy
+++ jenkins/booleanpipline.groovy
@@ -8,10 +8,10 @@ pipeline {
             steps {
                 script {
                     if (INCLUDE_DEBUG) {
-                        echo 'Debug information will be included in the deployment. false'
+                        echo 'Debug information will be included in the deployment. false 11'
                         // Add steps to include debug information
                     } else {
-                        echo 'Debug information will NOT be included in the deployment. true'
+                        echo 'Debug information will NOT be included in the deployment. true 22'
                         // Proceed without debug information
                     }
                 }
