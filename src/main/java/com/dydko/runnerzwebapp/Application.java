3202b51eb34158e633eacca9f19161a7bab78892
Miroslaw Dyduch <miroslaw.dyduch@gmail.com>
2025-03-20 08:30:33 +0100

    base sql query template

--- src/main/java/com/dydko/runnerzwebapp/Application.java
+++ src/main/java/com/dydko/runnerzwebapp/Application.java
@@ -1,31 +1,20 @@
 package com.dydko.runnerzwebapp;
 
-import com.dydko.runnerzwebapp.config.PizzaConfig;
 import lombok.extern.java.Log;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @Log
-public class Application implements CommandLineRunner {
 
-    private final PizzaConfig pizzaConfig;
-
-    public Application(PizzaConfig pizzaConfig) {
-        this.pizzaConfig = pizzaConfig;
-    }
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        log.info(String.format("I want a %s crust pizza, with %s and sauce %s",
-                pizzaConfig.getCrust(),
-                pizzaConfig.getTopping(),
-                pizzaConfig.getSauce()
-                ));
-    }
 }
