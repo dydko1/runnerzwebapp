34ded5c71eb2c3a68a071a74f339f7ae8d2379c6
Miroslaw Dyduch <miroslaw.dyduch@gmail.com>
2025-03-20 06:08:11 +0100

    lesson configuration configuration

--- src/main/java/com/dydko/runnerzwebapp/Application.java
+++ src/main/java/com/dydko/runnerzwebapp/Application.java
@@ -1,12 +1,31 @@
 package com.dydko.runnerzwebapp;
 
+import com.dydko.runnerzwebapp.config.PizzaConfig;
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+@Log
+public class Application implements CommandLineRunner {
+
+    private final PizzaConfig pizzaConfig;
+
+    public Application(PizzaConfig pizzaConfig) {
+        this.pizzaConfig = pizzaConfig;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info(String.format("I want a %s crust pizza, with %s and sauce %s",
+                pizzaConfig.getCrust(),
+                pizzaConfig.getTopping(),
+                pizzaConfig.getSauce()
+                ));
     }
 }
