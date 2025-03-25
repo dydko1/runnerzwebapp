e468fc9893bbbdae2c782f647fdd0f091b8248cd
Miroslaw Dyduch <miroslaw.dyduch@gmail.com>
2025-03-25 12:18:57 +0100

    trash 2

--- src/main/java/com/dydko/runnerzwebapp/Application.java
+++ src/main/java/com/dydko/runnerzwebapp/Application.java
@@ -1,12 +1,8 @@
 package com.dydko.runnerzwebapp;
 
 import lombok.extern.java.Log;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 @Log
